ef9cc6004b2c46b6b49525452e5667dc7ea084ac
Sckaletz <allan@leeloo.dk>
2024-08-21 20:51:47 +0200

    Startup

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees { get; set; }
+}
