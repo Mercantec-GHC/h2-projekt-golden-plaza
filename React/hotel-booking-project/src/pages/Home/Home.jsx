49be65b8757daf54502ec6369fc054b2fa29f9cb
rasmussckaletz <rasmus@leeloo.dk>
2024-09-02 13:56:55 +0200

    added checking current auth state, if logged in or not

--- React/hotel-booking-project/src/pages/Home/Home.jsx
+++ React/hotel-booking-project/src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
 import { Link } from "react-router-dom";
 
 function Home() {
+    const token = localStorage.getItem("token");
+
     return (
         <>
             <div>HomePage</div>
             <Link to="/signup">Sign Up</Link>
             <Link to="/login">Log In</Link>
+
+            {token ? (
+                <p>Logged in</p>
+            ) : (
+                <p>Not logged in</p>
+            )}
         </>
     )
 }
