af518d8b744d6e9c547f74acb468c6773353c321
Jonas <JMikkelsen40@outlook.com>
2024-09-23 13:45:12 +0200

    add keycloak to aspire

--- Aspire/Aspire.AppHost/Program.cs
+++ Aspire/Aspire.AppHost/Program.cs
@@ -4,10 +4,17 @@ var dbserver = builder.AddPostgres("dbserver");
 var db = dbserver.AddDatabase("hoteldb");
 dbserver.WithDataVolume().WithPgAdmin();
 
+
+// if keycloak fails, add the port number to it like this
+// var keycloak = builder.AddKeycloakContainer("keycloak", port: 8080).WithDataVolume();
+// default login is admin admin ( username: admin, paswword: admin ) can be changed
+var keycloak = builder.AddKeycloakContainer("keycloak").WithDataVolume();
+
 var api = builder.AddProject<Projects.API>("api").WithReference(db);
 
 builder.AddNpmApp("frontend", "../../React/hotel-booking-project")
     .WithReference(api)
+    .WithReference(keycloak)
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
@@ -15,6 +22,7 @@ builder.AddNpmApp("frontend", "../../React/hotel-booking-project")
 
 builder.AddNpmApp("admin-panel", "../../React/admin-panel")
     .WithReference(api)
+    .WithReference(keycloak)
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
