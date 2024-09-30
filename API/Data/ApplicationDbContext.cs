b9a205320690413b8764a958243a74b2900202ff
Rasmus Tanggaard <rasmus@tthost.dk>
2024-09-30 14:03:10 +0200

    Add controller for roomtype specifically

--- API/Data/ApplicationDbContext.cs
+++ API/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace API.Data;
 public class ApplicationDBContext : DbContext
 {
     public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomType> RoomType { get; set; }
     public DbSet<Booking> Bookings { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Employee> Employee { get; set; }
