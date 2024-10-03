a1cc41a4c176185d2d286f1726aee5f7ac7ea415
Rasmus Tanggaard <rasmus@tthost.dk>
2024-10-03 09:07:24 +0200

    Rename UserSid to UserId

--- DomainModels/Models/Ticket.cs
+++ DomainModels/Models/Ticket.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public Status status { get; set; }
-        public string userSid { get; set; }
+        public string UserId { get; set; }
 
     }
 
